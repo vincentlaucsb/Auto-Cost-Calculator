4d1a6a2a9bddebf1bc09ff7a234545bf41b70265
Vincent La <vincela9@gmail.com>
2019-11-11 15:54:49 -0800

    Split off main financial display into seperate file

--- src/components/AutoCostCalculator.tsx
+++ src/components/AutoCostCalculator.tsx
@@ -3,10 +3,8 @@ import { saveAs } from 'file-saver';
 import { ModalContainer, Modal } from "./Modal";
 import { Responsive, WidthProvider } from 'react-grid-layout';
 
-import { FuelType, FuelPrice, GasPriceChanger } from "./Fuel";
-import { Tabs } from "./Tabs";
+import { FuelPrice, GasPriceChanger } from "./Fuel";
 
-import { Table } from "./Car/Table";
 import { CarDatabase } from "./CarDatabase";
 import { Car } from "./Car/Car";
 import { CarList as CarList } from "./Car/List";
@@ -21,9 +19,6 @@ interface AutoCostCalcProps {
 };
 
 interface AutoCostCalcState extends AutoCostCalcProps{
-    months: number;
-    annualMileage: number;
-    activeTab: string;
     modalsVisible: Map<string, boolean>;
 };
 
@@ -41,9 +36,6 @@ export class AutoCostCalculator extends React.Component<AutoCostCalcProps, AutoC
         this.state = {
             data: props.data,
             ppg: props.ppg,
-            months: 48,
-            annualMileage: 12000,
-            activeTab: "Chart",
             modalsVisible: temp_modals_visible
         };
 
@@ -51,13 +43,10 @@ export class AutoCostCalculator extends React.Component<AutoCostCalcProps, AutoC
 
         this.updateCar = this.updateCar.bind(this);
         this.updateGasPrice = this.updateGasPrice.bind(this);
-        this.updateMileage = this.updateMileage.bind(this);
-        this.updateMonths = this.updateMonths.bind(this);
         this.addCar = this.addCar.bind(this);
         this.removeAll = this.removeAll.bind(this);
         this.removeCar = this.removeCar.bind(this);
         this.reset = this.reset.bind(this);
-        this.setActive = this.setActive.bind(this);
         this.save = this.save.bind(this);
         this.loadFile = this.loadFile.bind(this);
         this.saveFile = this.saveFile.bind(this);
@@ -66,18 +55,6 @@ export class AutoCostCalculator extends React.Component<AutoCostCalcProps, AutoC
     updateGasPrice(_ppg: FuelPrice) {
         this.setState({
             ppg: _ppg
-        });
-    }
-
-    updateMileage(mileage: number) {
-        this.setState({
-            annualMileage: mileage
-        });
-    }
-
-    updateMonths(_months: number) {
-        this.setState({
-            months: _months
         });
     }
 
@@ -103,12 +80,6 @@ export class AutoCostCalculator extends React.Component<AutoCostCalcProps, AutoC
     removeCar(id: number) {
         this.state.data.removeCar(id);
         this.setState({ data: this.state.data });
-    }
-
-    setActive(name: string) {
-        this.setState({
-            activeTab: name
-        });
     }
 
     // Reset the current calculator state
@@ -169,77 +140,7 @@ export class AutoCostCalculator extends React.Component<AutoCostCalcProps, AutoC
         saveAs(blob, "auto-cost-data.json");
     }
 
-    makeGraphData(): Map<string, Array<object>> {
-        let cars = this.state.data.toArray();
-
-        // a mapping of car names to arrays of (x, y) pairs
-        let data = new Map<string, Array<object>>();
-
-        for (let i in cars) {
-            let car = cars[i];
-
-            // An array of car costs, site-indexed by month
-            let costs = [];
-
-            for (var j = 0; j < this.state.months; j++) {
-                costs.push({
-                    x: j,
-                    y: car.costToDriveMonth(this.state.annualMileage / 12, j, this.state.ppg)
-                });
-            }
-
-            data.set(car.name, costs);
-        }
-
-        return data;
-    }
-
     render() {
-        const tabItems: Array<string> = [
-            "Chart", "Table"
-        ];
-
-        let body;
-
-        if (this.state.activeTab == "Chart") {
-            const Graph = React.lazy(() => import("./charts/Graph"));
-            const MileageChanger = React.lazy(() => import("./charts/MileageChanger"));
-            const MonthChanger = React.lazy(() => import("./charts/MonthChanger"));
-
-            body = <div style={{
-                width: '100%',
-                height: '100%',
-                display: 'flex',
-                flexDirection: 'column'
-            }}>
-                <React.Suspense fallback={<div>Loading...</div>}>
-                <Graph
-                    data={this.makeGraphData()}
-                />
-                <div style={{
-                    width: '100%',
-                    display: 'flex',
-                    alignContent: 'space-around',
-                    flexDirection: 'row'
-                }}>
-                    <div style={{ width: '72.5%' }}>
-                        <MileageChanger mileage={this.state.annualMileage} updateMileage={this.updateMileage} />
-                    </div>
-                    <div style={{width: '22.5%'}}>
-                        <MonthChanger months={this.state.months} updateMonths={this.updateMonths} />
-                    </div>
-                    </div>
-                </React.Suspense>
-            </div>;
-        } else {
-            body = <Table
-                annualMileage={this.state.annualMileage}
-                months={this.state.months}
-                data={this.state.data}
-                ppg={this.state.ppg}
-            />;
-        }
-
         // Grid
         var layouts = {
             lg: [
@@ -248,6 +149,8 @@ export class AutoCostCalculator extends React.Component<AutoCostCalcProps, AutoC
                 { i: 'c', x: 20, y: 2, w: 10, h: 40 }
             ]
         };
+
+        let MainDisplay = React.lazy(() => import("./MainDisplay"));
 
         return <React.Fragment>
             <ModalContainer />
@@ -289,18 +192,11 @@ export class AutoCostCalculator extends React.Component<AutoCostCalcProps, AutoC
                     // verticalCompact={false}
                 >
                     <div key="a">
-                        <div className="card" id="graph-panel" style={{ height: "100%" }}>
-                            <div className="card-header">
-                                <Tabs items={tabItems}
-                                    activeItem={this.state.activeTab}
-                                    setActive={this.setActive}
-                                />
-                            </div>
-                            <div className="card-body">
-                                {body}
-                            </div>
-                        </div>
+                        <React.Suspense fallback={<div>Loading...</div>}>
+                        <MainDisplay data={this.state.data} ppg={this.state.ppg} />
+                        </React.Suspense>
                     </div>
+
                     <div key="gas">
                         <GasPriceChanger ppg={this.state.ppg} updateGasPrice={this.updateGasPrice} />
                     </div>
